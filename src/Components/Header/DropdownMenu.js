e8282cfb682287e720e3d5e8da62dd6ced0d5513
Brendon Rodrigues <rodriguesbrendon861@gmail.com>
2022-07-19 19:16:27 -0300

    ainda consertando erros

--- src/Components/Header/DropdownMenu.js
+++ src/Components/Header/DropdownMenu.js
@@ -5,10 +5,10 @@ const DropDownMenu = ()=>{
     return(
             <S.DropDownList>
                 <S.DropDownItem><Link to="/Works">Mostrar todos </Link> </S.DropDownItem>
-                <S.DropDownItem><a href="https://agendaparadaangelica2030.netlify.app/" target="_blank">Agenda 2030 </a> </S.DropDownItem>
-                <S.DropDownItem><a href="https://ueefgv.csb.app/" target="_blank">ToDoFlix </a> </S.DropDownItem>
-                <S.DropDownItem><a href="https://n2fcrd.csb.app/" target="_blank">PetApi </a> </S.DropDownItem>
-                <S.DropDownItem><a href="https://brendonrodri.github.io/Basquiat/" target="_blank">Basquiat </a> </S.DropDownItem>
+                <S.DropDownItem><a href="https://agendaparadaangelica2030.netlify.app/">Agenda 2030 </a> </S.DropDownItem>
+                <S.DropDownItem><a href="https://ueefgv.csb.app/">ToDoFlix </a> </S.DropDownItem>
+                <S.DropDownItem><a href="https://n2fcrd.csb.app/">PetApi </a> </S.DropDownItem>
+                <S.DropDownItem><a href="https://brendonrodri.github.io/Basquiat/">Basquiat </a> </S.DropDownItem>
             </S.DropDownList>     
     )
 }
