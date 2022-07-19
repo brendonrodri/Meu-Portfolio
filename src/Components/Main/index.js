64377a408660c921dcb64c0be9c3cbd9b2f8e2ab
Brendon Rodrigues <79549808+brendonrodri@users.noreply.github.com>
2022-07-19 19:27:49 -0300

    Update index.js

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React from "react"
 import { Link } from "react-router-dom"
 import * as S from "../styles"
 import ProfilePic from "../Assets/Profile.png"
